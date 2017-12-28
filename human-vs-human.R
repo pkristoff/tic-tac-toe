74005a05096f9e306a334a41ff37f566c75ec66a
Paul Kristoff <paul.kristoff@kristoffs.com>
2017-12-28 15:07:19 -0500

    Moved common code to CreateTTTTree package.

--- human-vs-human.R
+++ human-vs-human.R
@@ -1,138 +1,9 @@
 
 
-# install.packages("shiny")
-library(shiny)
-# install.packages("shinyjs")
-library(shinyjs)
-
-# =================common=================
-board.size = 3
-
-# Checking whether somebody has won
-EvaluateBoard <- function(board) {
-  FlipMatrix <- function(m) {
-    apply(m, 2, rev)
-  }
-  sums <- c(colSums(board),
-            rowSums(board),
-            sum(diag(board)),
-            sum(diag(FlipMatrix(board))))
-
-  if (max(sums) == board.size) {
-    return(1)
-  }
-  if (min(sums) == -board.size) {
-    return(-1)
-  }
-  0
-}
-GenerateEmptyBoard <- function() {
-  matrix(0, ncol = board.size, nrow = board.size)
-}
-
-IsMovePossible <- function(board) {
-  length(which(board == 0)) > 0
-}
-# =======================================
-
+library(tttShiny)
 library(xtable)
 
-cbCol1 <- 3
-cbCol2 <- 3
-cbCol3 <- 3
-ui <- function () {
-  pageWithSidebar(
-    headerPanel("Tic Tac Toe"),
-
-    sidebarPanel(shinyjs::useShinyjs(),
-                 fixedRow(
-                   column(
-                     9,
-                     style = "background-color:pink;",
-                     fixedRow(
-                       column(
-                         cbCol1,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_1_1_X", "X", FALSE),
-                         checkboxInput("Cell_1_1_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol2,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_1_2_X", "X", FALSE),
-                         checkboxInput("Cell_1_2_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol3,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_1_3_X", "X", FALSE),
-                         checkboxInput("Cell_1_3_O", "O", FALSE)
-                       )
-                     ),
-                     fixedRow(
-                       column(
-                         cbCol1,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_2_1_X", "X", FALSE),
-                         checkboxInput("Cell_2_1_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol2,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_2_2_X", "X", FALSE),
-                         checkboxInput("Cell_2_2_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol3,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_2_3_X", "X", FALSE),
-                         checkboxInput("Cell_2_3_O", "O", FALSE)
-                       )
-                     ),
-                     fixedRow(
-                       column(
-                         cbCol1,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_3_1_X", "X", FALSE),
-                         checkboxInput("Cell_3_1_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol2,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_3_2_X", "X", FALSE),
-                         checkboxInput("Cell_3_2_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol3,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_3_3_X", "X", FALSE),
-                         checkboxInput("Cell_3_3_O", "O", FALSE)
-                       )
-                     )
-                   )
-                 )),
-
-    mainPanel(fixedRow(
-      column(
-        12,
-        #style = "background-color:pink;",
-        fixedRow(column(3, style = "background-color:aqua;", uiOutput('board'))),
-        fixedRow(column(3, style = "background-color:aqua;", textOutput('xWinsText'))),
-        fixedRow(column(3, style = "background-color:aqua;", textOutput('oWinsText'))),
-        fixedRow(column(3, style = "background-color:aqua;", textOutput('tiesText'))),
-        fixedRow(column(
-          3, style = "background-color:aqua;", actionButton("startNewGame", "Start New Game")
-        ))
-      )
-    ))
-  )
-}
+board.size = tttShiny::BoardSize()
 
 server <- function(input, output, session) {
   xSide <- -1
@@ -169,102 +40,6 @@ server <- function(input, output, session) {
     }
   }
 
-  #' Connects the server side with
-  #'
-  #' @param input: shiny input
-  #'
-  #' @examples
-  #'
-  #'
-  UpdateBoard <- function(input) {
-
-    #' converts boolean to board value
-    #'
-    #' @param isX: X has been selected
-    #'        isO: O has been selected
-    #'
-    #' @return Integer: 1 (isX), -1 (isO), 0
-    #'
-    #' @examples
-    #'
-    #'
-    GetSide <- function(isX, isO) {
-      if (isX) {
-        -1
-      } else if (isO)
-      {
-        1
-      } else{
-        0
-      }
-    }
-    reactive({
-      board <- GenerateEmptyBoard()
-      board[[1]] <- GetSide(input$Cell_1_1_X, input$Cell_1_1_O)
-      board[[4]] <- GetSide(input$Cell_1_2_X, input$Cell_1_2_O)
-      board[[7]] <- GetSide(input$Cell_1_3_X, input$Cell_1_3_O)
-      board[[2]] <- GetSide(input$Cell_2_1_X, input$Cell_2_1_O)
-      board[[5]] <- GetSide(input$Cell_2_2_X, input$Cell_2_2_O)
-      board[[8]] <- GetSide(input$Cell_2_3_X, input$Cell_2_3_O)
-      board[[3]] <- GetSide(input$Cell_3_1_X, input$Cell_3_1_O)
-      board[[6]] <- GetSide(input$Cell_3_2_X, input$Cell_3_2_O)
-      board[[9]] <- GetSide(input$Cell_3_3_X, input$Cell_3_3_O)
-      board
-    })
-  }
-
-  #'  disable & enable whole board.
-  #'
-  #' @param enableWidget: Boolean: TRUE: disables all checkboxes - game over
-  #'                              FALSE: enables all checkboxes - start new game.
-  #'
-  #' @return
-  #'
-  #' @examples
-  #'
-  #'
-  DisableWholeBoard <- function(localBoard, enableWidget) {
-    for (index in 1:9) {
-      cells = UICellForIndex(index)
-      if (enableWidget) {
-        shinyjs::disable(cells[[1]])
-        shinyjs::disable(cells[[2]])
-      } else {
-        shinyjs::enable(cells[[1]])
-        shinyjs::enable(cells[[2]])
-      }
-    }
-  }
-
-  #'  disables/enables checkboxes based on whether the XorO has been chosen and who's turn it is.
-  #'  if cell on local board has been chosen then disable.
-  #'  based on whose turn it is then enable for that player.
-  #'
-  #' @param localBoard: vector of Integer (1,0,-1)
-  #'
-  #' @return
-  #'
-  #' @examples
-  #'
-  #'
-  DisableBoard <- function(localBoard, nextPlayer) {
-    for (index in 1:9) {
-      cells = UICellForIndex(index)
-      if (localBoard[index] != 0) { # someone has played this cell.
-        shinyjs::disable(cells[[1]])
-        shinyjs::disable(cells[[2]])
-      } else {
-        if (nextPlayer == xSide) {
-          shinyjs::enable(cells[[1]])
-          shinyjs::disable(cells[[2]])
-        } else if (nextPlayer == oSide) {
-          shinyjs::enable(cells[[2]])
-          shinyjs::disable(cells[[1]])
-        }
-      }
-    }
-  }
-
   #' Resets for new game.
   #'
   #' @param
@@ -276,7 +51,7 @@ server <- function(input, output, session) {
   #'
   ResetGame <- function() {
     print("ResetGame")
-    DisableWholeBoard(localBoard, FALSE)
+    tttShiny::DisableWholeBoard(localBoard, FALSE)
     humanSide <<- 0
     humanSide2 <<- 0
     for (index in 1:9) {
@@ -345,12 +120,12 @@ server <- function(input, output, session) {
         # print(paste("winner=oSide"))
         oWins <<- oWins + 1
       }
-      DisableWholeBoard(localBoard, TRUE)
+      tttShiny::DisableWholeBoard(localBoard, TRUE)
       gameOver <- TRUE
-    } else if (!IsMovePossible(localBoard)) {
+    } else if (!tttShiny::IsMovePossible(localBoard)) {
       # print(paste("winner=tie"))
       ties <<- ties + 1
-      DisableWholeBoard(localBoard, TRUE)
+      tttShiny::DisableWholeBoard(localBoard, TRUE)
       gameOver <- TRUE
     }
     output$xWinsText <- renderText(paste("X Wins: ", xWins))
@@ -359,7 +134,7 @@ server <- function(input, output, session) {
     gameOver
   }
 
-  board <- UpdateBoard(input)
+  board <- tttShiny::UpdateBoard(input, xSide, oSide)
 
   output$board <- renderTable({
     localBoard <- board()
@@ -394,10 +169,10 @@ server <- function(input, output, session) {
         nextPlayer
       )
     )
-    winner <- EvaluateBoard(localBoard)
+    winner <- tttShiny::EvaluateBoard(localBoard)
     gameOver <- UpdateWins(winner, localBoard)
     if (!gameOver) {
-      DisableBoard(localBoard, nextPlayer)
+      tttShiny::DisableBoard(localBoard, nextPlayer*-1, xSide, oSide)
     }
     xtable(matrix(
       factor(
@@ -411,4 +186,4 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui = ui(), server = server)
+shinyApp(ui = tttShiny::shinyTTTUI(), server = server)
--- human-vs-tree.R
+++ human-vs-tree.R
@@ -1,144 +1,9 @@
 
-
-# install.packages("shiny")
-library(shiny)
-# install.packages("shinyjs")
-library(shinyjs)
-
-GenerateShinyBoard <- function(data) {
-  matrix(data, ncol = board.size, nrow = board.size)
-}
-
+library(tttShiny)
 library(CreateTTTTree)
 library(xtable)
 
-# =================common=================
-board.size = 3
-
-# Checking whether somebody has won
-EvaluateBoard <- function(board) {
-
-  FlipMatrix <- function(m) {
-    apply(m, 2, rev)
-  }
-  sums <- c(colSums(board),
-            rowSums(board),
-            sum(diag(board)),
-            sum(diag(FlipMatrix(board))))
-
-  if (max(sums) == board.size) {
-    return(1)
-  }
-  if (min(sums) == -board.size) {
-    return(-1)
-  }
-  0
-}
-GenerateEmptyBoard <- function() {
-  matrix(0, ncol = board.size, nrow = board.size)
-}
-
-IsMovePossible <- function(board) {
-  length(which(board == 0)) > 0
-}
-# =======================================
-
-cbCol1 <- 3
-cbCol2 <- 3
-cbCol3 <- 3
-ui <- function () {
-  pageWithSidebar(
-    headerPanel("Tic Tac Toe"),
-
-    sidebarPanel(shinyjs::useShinyjs(),
-                 fixedRow(
-                   column(
-                     9,
-                     style = "background-color:pink;",
-                     fixedRow(
-                       column(
-                         cbCol1,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_1_1_X", "X", FALSE),
-                         checkboxInput("Cell_1_1_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol2,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_1_2_X", "X", FALSE),
-                         checkboxInput("Cell_1_2_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol3,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_1_3_X", "X", FALSE),
-                         checkboxInput("Cell_1_3_O", "O", FALSE)
-                       )
-                     ),
-                     fixedRow(
-                       column(
-                         cbCol1,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_2_1_X", "X", FALSE),
-                         checkboxInput("Cell_2_1_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol2,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_2_2_X", "X", FALSE),
-                         checkboxInput("Cell_2_2_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol3,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_2_3_X", "X", FALSE),
-                         checkboxInput("Cell_2_3_O", "O", FALSE)
-                       )
-                     ),
-                     fixedRow(
-                       column(
-                         cbCol1,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_3_1_X", "X", FALSE),
-                         checkboxInput("Cell_3_1_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol2,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_3_2_X", "X", FALSE),
-                         checkboxInput("Cell_3_2_O", "O", FALSE)
-                       )
-                       ,
-                       column(
-                         cbCol3,
-                         style = "background-color:aqua;",
-                         checkboxInput("Cell_3_3_X", "X", FALSE),
-                         checkboxInput("Cell_3_3_O", "O", FALSE)
-                       )
-                     )
-                   )
-                 )),
-
-    mainPanel(fixedRow(
-      column(
-        12,
-        #style = "background-color:pink;",
-        fixedRow(column(3, style = "background-color:aqua;", uiOutput('board'))),
-        fixedRow(column(3, style = "background-color:aqua;", textOutput('xWinsText'))),
-        fixedRow(column(3, style = "background-color:aqua;", textOutput('oWinsText'))),
-        fixedRow(column(3, style = "background-color:aqua;", textOutput('tiesText'))),
-        fixedRow(column(
-          3, style = "background-color:aqua;", actionButton("startNewGame", "Start New Game")
-        ))
-      )
-    ))
-  )
-}
+board.size = tttShiny::BoardSize()
 
 server <- function(input, output, session) {
   xSide <- -1
@@ -148,29 +13,8 @@ server <- function(input, output, session) {
   xWins <- 0
   oWins <- 0
   ties <- 0
-  tree <- GenerateTable()
+  tree <- CreateTTTTree::GenerateTable()
   initialTree <- tree
-
-  IsMachineTurn <- function (board) {
-    # length(which(board == computerSide)) > length(which(board == humanSide))
-    if (computerSide != humanSide) {
-      numX <- 0
-      numO <- 0
-      for (rowNum in 1:3) {
-        for (colNum in 1:3) {
-          if (board[rowNum, colNum] == computerSide) {
-            numX <- numX + 1
-          } else if (board[rowNum, colNum] == humanSide) {
-            numO <- numO + 1
-          }
-        }
-      }
-      # print(paste("numX=", numX, "numO=", numO))
-      numX != numO
-    } else {
-      FALSE
-    }
-  }
   SetHumanSide <- function(board) {
     if (humanSide == 0) {
       xLen <- length(which(board == xSide))
@@ -185,96 +29,18 @@ server <- function(input, output, session) {
       # print(paste("humanSide=", humanSide, "xSide=", xSide, "oSide=", oSide))
     }
   }
-  UpdateBoard <- function(input) {
-    GetSide <- function(isX, isY) {
-      if (isX) {
-        xSide
-      } else if (isY)
-      {
-        oSide
-      } else{
-        0
-      }
-    }
-    reactive({
-      board <- GenerateEmptyBoard()
-      board[[1]] <- GetSide(input$Cell_1_1_X, input$Cell_1_1_O)
-      board[[4]] <- GetSide(input$Cell_1_2_X, input$Cell_1_2_O)
-      board[[7]] <- GetSide(input$Cell_1_3_X, input$Cell_1_3_O)
-      board[[2]] <- GetSide(input$Cell_2_1_X, input$Cell_2_1_O)
-      board[[5]] <- GetSide(input$Cell_2_2_X, input$Cell_2_2_O)
-      board[[8]] <- GetSide(input$Cell_2_3_X, input$Cell_2_3_O)
-      board[[3]] <- GetSide(input$Cell_3_1_X, input$Cell_3_1_O)
-      board[[6]] <- GetSide(input$Cell_3_2_X, input$Cell_3_2_O)
-      board[[9]] <- GetSide(input$Cell_3_3_X, input$Cell_3_3_O)
-      board
-    })
-  }
-  UpdateInputForMove <- function(localBoard, move, side) {
-    if (side == xSide) {
-      cellIndex <- 1
-      labelText <- "X"
-    } else {
-      cellIndex <- 2
-      labelText <- "O"
-    }
-    cell <- UICellForIndex(move)[[cellIndex]]
-    updateCheckboxInput(session, cell, label = labelText, value = TRUE)
-  }
-  DisableWholeBoard <- function(localBoard, enableWidget) {
-    for (index in 1:9) {
-      cells = UICellForIndex(index)
-      if (enableWidget) {
-        shinyjs::disable(cells[[1]])
-        shinyjs::disable(cells[[2]])
-      } else {
-        shinyjs::enable(cells[[1]])
-        shinyjs::enable(cells[[2]])
-      }
-    }
-  }
-  DisableBoard <- function(localBoard) {
-    for (index in 1:9) {
-      cells = UICellForIndex(index)
-      if (localBoard[index] != 0) {
-        shinyjs::disable(cells[[1]])
-        shinyjs::disable(cells[[2]])
-      }
-      if (computerSide == xSide) {
-        shinyjs::disable(cells[[1]])
-      } else if (computerSide == oSide) {
-        shinyjs::disable(cells[[2]])
-      }
-    }
-  }
 
   ResetGame <- function() {
     print("ResetGame")
     tree <<- initialTree
-    DisableWholeBoard(localBoard, FALSE)
+    tttShiny::DisableWholeBoard(localBoard, FALSE)
     humanSide <<- 0
     computerSide <<- 0
     for (index in 1:9) {
-      cells <- UICellForIndex(index)
-      updateCheckboxInput(session, cells[[1]], label = "X", value = FALSE)
-      updateCheckboxInput(session, cells[[2]], label = "O", value = FALSE)
+      cells <- tttShiny::UICellForIndex(index)
+      shiny::updateCheckboxInput(session, cells[[1]], label = "X", value = FALSE)
+      shiny::updateCheckboxInput(session, cells[[2]], label = "O", value = FALSE)
     }
-  }
-
-  UICellForIndex <- function(index) {
-    switch(
-      index,
-      list("Cell_1_1_X", "Cell_1_1_O"),
-      list("Cell_2_1_X", "Cell_2_1_O"),
-      list("Cell_3_1_X", "Cell_3_1_O"),
-      list("Cell_1_2_X", "Cell_1_2_O"),
-      list("Cell_2_2_X", "Cell_2_2_O"),
-      list("Cell_3_2_X", "Cell_3_2_O"),
-      list("Cell_1_3_X", "Cell_1_3_O"),
-      list("Cell_2_3_X", "Cell_2_3_O"),
-      list("Cell_3_3_X", "Cell_3_3_O"),
-      list("Unknown Cell X", "Unknown Cell O")
-    )
   }
 
   observeEvent(input$startNewGame, {
@@ -287,7 +53,7 @@ server <- function(input, output, session) {
     # print(paste("treemove Side=", side))
     # print(DisplayBoard(board))
     for (node in tree$tree) {
-      PrintNode(node)
+      CreateTTTTree::PrintNode(node)
       if (node$bestValueOppX > bestValue) {
         bestMoveNode <- node
         bestValue <- node$bestValueOppX
@@ -309,39 +75,39 @@ server <- function(input, output, session) {
     xxx
   }
 
-  PrintNode <- function(node, space = "") {
-    cat(
-      space,
-      "level=",
-      node$level,
-      "rootPick=[",
-      node$rootRow,
-      node$rootCol,
-      "]",
-      "sideMadeThisMove=",
-      node$sideMadeThisMove,
-      " bestValueMeX=",
-      node$bestValueMeX,
-      " bestValueOppX=",
-      node$bestValueOppX,
-      " numSubNodes=",
-      length(node$tree),
-      "\n"
-    )
-  }
-
   ComputerMove <-
     function(winner,
              localBoard,
              computerSide,
              tree) {
+
+      IsMachineTurn <- function (board) {
+        # length(which(board == computerSide)) > length(which(board == humanSide))
+        if (computerSide != humanSide) {
+          numX <- 0
+          numO <- 0
+          for (rowNum in 1:3) {
+            for (colNum in 1:3) {
+              if (board[rowNum, colNum] == computerSide) {
+                numX <- numX + 1
+              } else if (board[rowNum, colNum] == humanSide) {
+                numO <- numO + 1
+              }
+            }
+          }
+          # print(paste("numX=", numX, "numO=", numO))
+          numX != numO
+        } else {
+          FALSE
+        }
+      }
       if (winner == 0 &&
           IsMachineTurn(localBoard) && IsMovePossible(localBoard)) {
         # print(paste("Move happening: computerSide=", computerSide))
         x <- TreeMove(tree, localBoard, computerSide)
         localBoard <- x$localBoard
         move <- x$move
-        UpdateInputForMove(localBoard, move, computerSide)
+        tttShiny::UpdateInputForMove(localBoard, move, computerSide, xSide, session)
         x
       }
     }
@@ -356,12 +122,12 @@ server <- function(input, output, session) {
         # print(paste("winner=oSide"))
         oWins <<- oWins + 1
       }
-      DisableWholeBoard(localBoard, TRUE)
+      tttShiny::DisableWholeBoard(localBoard, TRUE)
       gameOver <- TRUE
     } else if (!IsMovePossible(localBoard)) {
       # print(paste("winner=tie"))
       ties <<- ties + 1
-      DisableWholeBoard(localBoard, TRUE)
+      tttShiny::DisableWholeBoard(localBoard, TRUE)
       gameOver <- TRUE
     }
     output$xWinsText <- renderText(paste("X Wins: ", xWins))
@@ -381,69 +147,34 @@ server <- function(input, output, session) {
         found <- node
       }
     }
-    # if (!is.null(found)) {
-      # PrintNode(found)
-      # print("exit gettree")
-    # } else {
-      # if (tree$level != 0){
-      #   vvv
-      # }
-      # PrintNode(tree)
-      # print("exit gettree no tree")
-    # }
     found
   }
-  PrintMove <- function(node, whoMoved) {
-    if (is.null(node)) {
-      cat("No Move by ", whoMoved, "\n")
-    } else {
-      cat(
-        "Move by ",
-        whoMoved,
-        " level=",
-        node$level,
-        "rootPick=[",
-        node$rootRow,
-        node$rootCol,
-        "]",
-        "sideMadeThisMove=",
-        node$sideMadeThisMove,
-        " bestValueMeX=",
-        node$bestValueMeX,
-        " bestValueOppX=",
-        node$bestValueOppX,
-        " numSubNodes=",
-        length(node$tree),
-        "\n"
-      )
-    }
-  }
 
-  board <- UpdateBoard(input)
+  board <- tttShiny::UpdateBoard(input, xSide, oSide)
 
   output$board <- renderTable({
     print("*************start****************")
     localBoard <- board()
     SetHumanSide(localBoard)
     localTree <- GetTree(localBoard, tree)
-    PrintMove(localTree, 'Human')
+    CreateTTTTree::PrintMove(localTree, 'Human')
     if (!is.null(localTree)) {
       tree <<- localTree
-      winner <- EvaluateBoard(localBoard)
+      winner <- tttShiny::EvaluateBoard(localBoard)
       # print(paste("Winner=",winner))
-      if (winner == 0 && IsMovePossible(localBoard)) {
+      if (winner == 0 && tttShiny::IsMovePossible(localBoard)) {
         x <<- ComputerMove(winner, localBoard, computerSide, tree)
         localBoard <- x$localBoard
         tree <<- x$tree
         # PrintNode(tree)
         winner <- EvaluateBoard(localBoard)
-        PrintMove(tree, 'Computer')
+        CreateTTTTree::PrintMove(tree, 'Computer')
       }
       gameOver <- UpdateWins(winner, localBoard)
       if (gameOver) {
         # tic.ai <<- Train(winner)
       } else {
-        DisableBoard(localBoard)
+        tttShiny::DisableBoard(localBoard, computerSide, xSide, oSide)
       }
     }
     print("*********end****************")
@@ -459,5 +190,5 @@ server <- function(input, output, session) {
   })
 }
 
-
-shinyApp(ui = ui(), server = server)
+print(paste("Testing"))
+shinyApp(ui = tttShiny::shinyTTTUI(), server = server)
